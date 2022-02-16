7ec250c89ada7ff696050db13c0d8d5371b3aef5
Kun Qin <kuqin@microsoft.com>
2022-02-16 20:25:59 +0000

    Merged PR 4874: Introduce freeze setup script
    Introduce freeze setup script for platforms to create binary files of ConfigEditor.

---
+++ SetupDataPkg/Tools/setup.py
@@ -0,0 +1,18 @@
+# @file
+# Setup script to freeze ConfigEditor.
+#
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+
+import os
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"]}
+
+setup(name = "MuConfigEditor",
+      version = "1.0",
+      description = "",
+      options = {"build_exe": build_exe_options},
+      executables = [Executable(os.path.join(os.path.dirname(os.path.realpath(__file__)), "ConfigEditor.py"))])
