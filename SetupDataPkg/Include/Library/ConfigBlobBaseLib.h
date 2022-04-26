0fcac8e40603083af6808c27ac2099a53268a37a
Kun Qin <kuqin@microsoft.com>
2022-04-25 20:49:16 -0700

    Exposed more interfaces

--- SetupDataPkg/Include/Library/ConfigBlobBaseLib.h
+++ SetupDataPkg/Include/Library/ConfigBlobBaseLib.h
@@ -13,6 +13,28 @@
 #include <Library/ConfigDataLib.h>
 
 /**
+  Find configuration data header by its tag and platform ID.
+
+  @param[in] ConfBlob    Pointer to configuration data blob to be looked at.
+  @param[in] PidMask     Platform ID mask.
+  @param[in] Tag         Configuration TAG ID to find.
+  @param[in] IsInternal  Search for internal data base only if it is non-zero.
+  @param[in] Level       Nested call level.
+
+  @retval             Configuration data header pointer.
+                      NULL if the tag cannot be found.
+
+**/
+CDATA_HEADER *
+FindConfigHdrByPidMaskTag (
+  VOID    *ConfBlob,
+  UINT32  PidMask,
+  UINT32  Tag,
+  UINT8   IsInternal,
+  UINT32  Level
+  );
+
+/**
   Find configuration data header by its tag.
 
   @param[in] ConfBlob    Pointer to configuration data blob to be looked at.
@@ -26,6 +48,25 @@ CDATA_HEADER *
 EFIAPI
 FindConfigHdrByTag (
   VOID    *ConfBlob,
+  UINT32  Tag
+  );
+
+/**
+  Find configuration data by its tag and PlatforID.
+
+  @param[in] ConfBlob       Pointer to configuration data blob to be looked at.
+  @param[in] PlatformId     Platform ID.
+  @param[in] Tag            Configuration TAG ID to find.
+
+  @retval            Configuration data pointer.
+                     NULL if the tag cannot be found.
+
+**/
+VOID *
+EFIAPI
+FindConfigDataByPidTag (
+  VOID    *ConfBlob,
+  UINT16  PlatformId,
   UINT32  Tag
   );
 
@@ -44,6 +85,21 @@ EFIAPI
 FindConfigDataByTag (
   VOID    *ConfBlob,
   UINT32  Tag
+  );
+
+/**
+  Get a full CFGDATA set length.
+
+  @param[in] ConfBlob    Pointer to configuration data blob to be looked at.
+
+  @retval   Length of a full CFGDATA set.
+            0 indicates no CFGDATA exists.
+
+**/
+UINT32
+EFIAPI
+GetConfigDataSize (
+  VOID  *ConfBlob
   );
 
 #endif
