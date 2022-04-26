88496b642805c91185a14aadd6cf4395efc28a6f
Kun Qin <kuqin@microsoft.com>
2022-04-25 21:32:47 -0700

    Added unit test for config blob base lib

--- SetupDataPkg/Include/Library/ConfigBlobBaseLib.h
+++ SetupDataPkg/Include/Library/ConfigBlobBaseLib.h
@@ -13,28 +13,6 @@
 #include <Library/ConfigDataLib.h>
 
 /**
-  Find configuration data header by its tag and platform ID.
-
-  @param[in] ConfBlob    Pointer to configuration data blob to be looked at.
-  @param[in] PidMask     Platform ID mask.
-  @param[in] Tag         Configuration TAG ID to find.
-  @param[in] IsInternal  Search for internal data base only if it is non-zero.
-  @param[in] Level       Nested call level.
-
-  @retval             Configuration data header pointer.
-                      NULL if the tag cannot be found.
-
-**/
-CDATA_HEADER *
-FindConfigHdrByPidMaskTag (
-  VOID    *ConfBlob,
-  UINT32  PidMask,
-  UINT32  Tag,
-  UINT8   IsInternal,
-  UINT32  Level
-  );
-
-/**
   Find configuration data header by its tag.
 
   @param[in] ConfBlob    Pointer to configuration data blob to be looked at.
@@ -48,25 +26,6 @@ CDATA_HEADER *
 EFIAPI
 FindConfigHdrByTag (
   VOID    *ConfBlob,
-  UINT32  Tag
-  );
-
-/**
-  Find configuration data by its tag and PlatforID.
-
-  @param[in] ConfBlob       Pointer to configuration data blob to be looked at.
-  @param[in] PlatformId     Platform ID.
-  @param[in] Tag            Configuration TAG ID to find.
-
-  @retval            Configuration data pointer.
-                     NULL if the tag cannot be found.
-
-**/
-VOID *
-EFIAPI
-FindConfigDataByPidTag (
-  VOID    *ConfBlob,
-  UINT16  PlatformId,
   UINT32  Tag
   );
 
