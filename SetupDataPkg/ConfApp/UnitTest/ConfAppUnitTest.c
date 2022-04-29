4cd7d5056299c7ed9db3956374a8ec494ec1053a
Kun Qin <kuqin@microsoft.com>
2022-04-28 23:20:14 -0700

    Now it really works

--- SetupDataPkg/ConfApp/UnitTest/ConfAppUnitTest.c
+++ SetupDataPkg/ConfApp/UnitTest/ConfAppUnitTest.c
@@ -212,131 +212,6 @@ Print (
   return Ret;
 }
 
-
-/**
-  Returns the value of a variable.
-
-  @param[in]       VariableName  A Null-terminated string that is the name of the vendor's
-                                 variable.
-  @param[in]       VendorGuid    A unique identifier for the vendor.
-  @param[out]      Attributes    If not NULL, a pointer to the memory location to return the
-                                 attributes bitmask for the variable.
-  @param[in, out]  DataSize      On input, the size in bytes of the return Data buffer.
-                                 On output the size of data returned in Data.
-  @param[out]      Data          The buffer to return the contents of the variable. May be NULL
-                                 with a zero DataSize in order to determine the size buffer needed.
-
-  @retval EFI_SUCCESS            The function completed successfully.
-  @retval EFI_NOT_FOUND          The variable was not found.
-  @retval EFI_BUFFER_TOO_SMALL   The DataSize is too small for the result.
-  @retval EFI_INVALID_PARAMETER  VariableName is NULL.
-  @retval EFI_INVALID_PARAMETER  VendorGuid is NULL.
-  @retval EFI_INVALID_PARAMETER  DataSize is NULL.
-  @retval EFI_INVALID_PARAMETER  The DataSize is not too small and Data is NULL.
-  @retval EFI_DEVICE_ERROR       The variable could not be retrieved due to a hardware error.
-  @retval EFI_SECURITY_VIOLATION The variable could not be retrieved due to an authentication failure.
-
-**/
-STATIC
-EFI_STATUS
-EFIAPI
-MockGetVariable (
-  IN     CHAR16                      *VariableName,
-  IN     EFI_GUID                    *VendorGuid,
-  OUT    UINT32                      *Attributes     OPTIONAL,
-  IN OUT UINTN                       *DataSize,
-  OUT    VOID                        *Data           OPTIONAL
-  )
-{
-  UINTN Size;
-  VOID  *RetData;
-
-  assert_non_null (VariableName);
-  assert_memory_equal (VendorGuid, PcdGetPtr (PcdConfigPolicyVariableGuid), sizeof (EFI_GUID));
-  assert_non_null (DataSize);
-
-  DEBUG ((DEBUG_INFO, "%a Name: %s, GUID: %g, Size: %x\n", __FUNCTION__, VariableName, VendorGuid, *DataSize));
-
-  check_expected (VariableName);
-  Size = (UINTN)mock();
-  if (Size > *DataSize) {
-    *DataSize = Size;
-    return EFI_BUFFER_TOO_SMALL;
-  } else {
-    *DataSize = Size;
-    RetData = (VOID*)mock();
-    CopyMem (Data, RetData, Size);
-  }
-
-  return (EFI_STATUS)mock ();
-}
-
-/**
-  Sets the value of a variable.
-
-  @param[in]  VariableName       A Null-terminated string that is the name of the vendor's variable.
-                                 Each VariableName is unique for each VendorGuid. VariableName must
-                                 contain 1 or more characters. If VariableName is an empty string,
-                                 then EFI_INVALID_PARAMETER is returned.
-  @param[in]  VendorGuid         A unique identifier for the vendor.
-  @param[in]  Attributes         Attributes bitmask to set for the variable.
-  @param[in]  DataSize           The size in bytes of the Data buffer. Unless the EFI_VARIABLE_APPEND_WRITE or
-                                 EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS attribute is set, a size of zero
-                                 causes the variable to be deleted. When the EFI_VARIABLE_APPEND_WRITE attribute is
-                                 set, then a SetVariable() call with a DataSize of zero will not cause any change to
-                                 the variable value (the timestamp associated with the variable may be updated however
-                                 even if no new data value is provided,see the description of the
-                                 EFI_VARIABLE_AUTHENTICATION_2 descriptor below. In this case the DataSize will not
-                                 be zero since the EFI_VARIABLE_AUTHENTICATION_2 descriptor will be populated).
-  @param[in]  Data               The contents for the variable.
-
-  @retval EFI_SUCCESS            The firmware has successfully stored the variable and its data as
-                                 defined by the Attributes.
-  @retval EFI_INVALID_PARAMETER  An invalid combination of attribute bits, name, and GUID was supplied, or the
-                                 DataSize exceeds the maximum allowed.
-  @retval EFI_INVALID_PARAMETER  VariableName is an empty string.
-  @retval EFI_OUT_OF_RESOURCES   Not enough storage is available to hold the variable and its data.
-  @retval EFI_DEVICE_ERROR       The variable could not be retrieved due to a hardware error.
-  @retval EFI_WRITE_PROTECTED    The variable in question is read-only.
-  @retval EFI_WRITE_PROTECTED    The variable in question cannot be deleted.
-  @retval EFI_SECURITY_VIOLATION The variable could not be written due to EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACESS being set,
-                                 but the AuthInfo does NOT pass the validation check carried out by the firmware.
-
-  @retval EFI_NOT_FOUND          The variable trying to be updated or deleted was not found.
-
-**/
-STATIC
-EFI_STATUS
-EFIAPI
-MockSetVariable (
-  IN  CHAR16                       *VariableName,
-  IN  EFI_GUID                     *VendorGuid,
-  IN  UINT32                       Attributes,
-  IN  UINTN                        DataSize,
-  IN  VOID                         *Data
-  )
-{
-  assert_non_null (VariableName);
-  assert_memory_equal (VendorGuid, PcdGetPtr (PcdConfigPolicyVariableGuid), sizeof (EFI_GUID));
-  assert_int_equal (Attributes, CDATA_NV_VAR_ATTR);
-
-  DEBUG ((DEBUG_INFO, "%a Name: %s\n", __FUNCTION__, VariableName));
-
-  check_expected (VariableName);
-  check_expected (DataSize);
-  check_expected (Data);
-
-  return (EFI_STATUS)mock ();
-}
-
-///
-/// Mock version of the UEFI Runtime Services Table
-///
-EFI_RUNTIME_SERVICES  MockRuntime = {
-  .GetVariable = MockGetVariable,
-  .SetVariable = MockSetVariable,
-};
-
 EFI_STATUS
 EFIAPI
 MockLocateProtocol (
@@ -390,30 +265,6 @@ MockSetWatchdogTimer (
   return (EFI_STATUS)mock();
 }
 
-// EFI_STATUS
-// EFIAPI
-// MockCreateEvent (
-//   IN  UINT32                       Type,
-//   IN  EFI_TPL                      NotifyTpl,
-//   IN  EFI_EVENT_NOTIFY             NotifyFunction,
-//   IN  VOID                         *NotifyContext,
-//   OUT EFI_EVENT                    *Event
-//   )
-// {
-//   return 
-// }
-
-// EFI_STATUS
-// EFIAPI
-// MockSetTimer (
-//   IN  EFI_EVENT                Event,
-//   IN  EFI_TIMER_DELAY          Type,
-//   IN  UINT64                   TriggerTime
-//   )
-// {
-
-// }
-
 EFI_STATUS
 EFIAPI
 MockWaitForEvent (
@@ -458,92 +309,14 @@ EFI_BOOT_SERVICES  MockBoot = {
   .CloseEvent = MockCloseEvent,
 };
 
-// /**
-//   Unit test for ColdReset () API of the ConfDataSettingProvider.
-
-//   @param[in]  Context    [Optional] An optional parameter that enables:
-//                          1) test-case reuse with varied parameters and
-//                          2) test-case re-entry for Target tests that need a
-//                          reboot.  This parameter is a VOID* and it is the
-//                          responsibility of the test author to ensure that the
-//                          contents are well understood by all test cases that may
-//                          consume it.
-
-//   @retval  UNIT_TEST_PASSED             The Unit test has completed and the test
-//                                         case was successful.
-//   @retval  UNIT_TEST_ERROR_TEST_FAILED  A test case assertion has failed.
-// **/
-// UNIT_TEST_STATUS
-// EFIAPI
-// ResetColdShouldIssueAColdReset (
-//   IN UNIT_TEST_CONTEXT  Context
-//   )
-// {
-//   expect_value (MockResetSystem, ResetType, EfiResetCold);
-//   expect_value (MockResetSystem, ResetStatus, EFI_SUCCESS);
-
-//   ResetCold ();
-
-//   return UNIT_TEST_PASSED;
-// }
-
-// /**
-//   Unit test for WarmReset () API of the ConfDataSettingProvider.
-
-//   @param[in]  Context    [Optional] An optional parameter that enables:
-//                          1) test-case reuse with varied parameters and
-//                          2) test-case re-entry for Target tests that need a
-//                          reboot.  This parameter is a VOID* and it is the
-//                          responsibility of the test author to ensure that the
-//                          contents are well understood by all test cases that may
-//                          consume it.
-
-//   @retval  UNIT_TEST_PASSED             The Unit test has completed and the test
-//                                         case was successful.
-//   @retval  UNIT_TEST_ERROR_TEST_FAILED  A test case assertion has failed.
-// **/
-// UNIT_TEST_STATUS
-// EFIAPI
-// ResetWarmShouldIssueAWarmReset (
-//   IN UNIT_TEST_CONTEXT  Context
-//   )
-// {
-//   expect_value (MockResetSystem, ResetType, EfiResetWarm);
-//   expect_value (MockResetSystem, ResetStatus, EFI_SUCCESS);
-
-//   ResetWarm ();
-
-//   return UNIT_TEST_PASSED;
-// }
-
-// /**
-//   Unit test for ResetShutdown () API of the ConfDataSettingProvider.
-
-//   @param[in]  Context    [Optional] An optional parameter that enables:
-//                          1) test-case reuse with varied parameters and
-//                          2) test-case re-entry for Target tests that need a
-//                          reboot.  This parameter is a VOID* and it is the
-//                          responsibility of the test author to ensure that the
-//                          contents are well understood by all test cases that may
-//                          consume it.
-
-//   @retval  UNIT_TEST_PASSED             The Unit test has completed and the test
-//                                         case was successful.
-//   @retval  UNIT_TEST_ERROR_TEST_FAILED  A test case assertion has failed.
-// **/
-// UNIT_TEST_STATUS
-// EFIAPI
-// ResetShutdownShouldIssueAShutdown (
-//   IN UNIT_TEST_CONTEXT  Context
-//   )
-// {
-//   expect_value (MockResetSystem, ResetType, EfiResetShutdown);
-//   expect_value (MockResetSystem, ResetStatus, EFI_SUCCESS);
-
-//   ResetShutdown ();
-
-//   return UNIT_TEST_PASSED;
-// }
+VOID
+EFIAPI
+ConfAppCleanup (
+  IN UNIT_TEST_CONTEXT  Context
+  )
+{
+  mConfState = 0;
+}
 
 /**
   Unit test for ConfAppEntry of ConfApp when selecting 1.
@@ -989,14 +762,14 @@ UnitTestingEntry (
   //
   // --------------Suite-----------Description--------------Name----------Function--------Pre---Post-------------------Context-----------
   //
-  AddTestCase (MiscTests, "ConfApp Select 1 should go to System Info", "Select1", ConfAppEntrySelect1, NULL, NULL, NULL);
+  AddTestCase (MiscTests, "ConfApp Select 1 should go to System Info", "Select1", ConfAppEntrySelect1, NULL, ConfAppCleanup, NULL);
   // TODO: this is not supported yet
-  // AddTestCase (MiscTests, "ConfApp Select 2 should go to Secure Boot", "Select2", ConfAppEntrySelect2, NULL, NULL, NULL);
-  AddTestCase (MiscTests, "ConfApp Select 3 should go to Boot Options", "Select3", ConfAppEntrySelect3, NULL, NULL, NULL);
-  AddTestCase (MiscTests, "ConfApp Select 4 should go to Setup Option", "Select4", ConfAppEntrySelect4, NULL, NULL, NULL);
-  AddTestCase (MiscTests, "ConfApp Select h should reprint the options", "SelectH", ConfAppEntrySelectH, NULL, NULL, NULL);
-  AddTestCase (MiscTests, "ConfApp Select ESC should confirm reboot", "SelectEsc", ConfAppEntrySelectEsc, NULL, NULL, NULL);
-  AddTestCase (MiscTests, "ConfApp Select other should do nothing", "SelectOther", ConfAppEntrySelectOther, NULL, NULL, NULL);
+  // AddTestCase (MiscTests, "ConfApp Select 2 should go to Secure Boot", "Select2", ConfAppEntrySelect2, NULL, ConfAppCleanup, NULL);
+  AddTestCase (MiscTests, "ConfApp Select 3 should go to Boot Options", "Select3", ConfAppEntrySelect3, NULL, ConfAppCleanup, NULL);
+  AddTestCase (MiscTests, "ConfApp Select 4 should go to Setup Option", "Select4", ConfAppEntrySelect4, NULL, ConfAppCleanup, NULL);
+  AddTestCase (MiscTests, "ConfApp Select h should reprint the options", "SelectH", ConfAppEntrySelectH, NULL, ConfAppCleanup, NULL);
+  AddTestCase (MiscTests, "ConfApp Select ESC should confirm reboot", "SelectEsc", ConfAppEntrySelectEsc, NULL, ConfAppCleanup, NULL);
+  AddTestCase (MiscTests, "ConfApp Select other should do nothing", "SelectOther", ConfAppEntrySelectOther, NULL, ConfAppCleanup, NULL);
 
   //
   // Execute the tests.
