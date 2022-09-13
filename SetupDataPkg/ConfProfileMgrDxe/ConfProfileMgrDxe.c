db04814e2a58c177e74f8c392fa1e598a0d2f8cd
Oliver Smith-Denny <osde@microsoft.com>
2022-09-13 10:53:08 -0700

    Actually build before pushing

--- SetupDataPkg/ConfProfileMgrDxe/ConfProfileMgrDxe.c
+++ SetupDataPkg/ConfProfileMgrDxe/ConfProfileMgrDxe.c
@@ -257,7 +257,7 @@ ConfProfileMgrDxeEntry (
       // if we didn't find the profile and it is not the generic profile, it is invalid and
       // we default to the generic profile
       if ((!FoundProfileInList) &&
-          ((0 != CompareMem (&ActiveProfileGuid, (EFI_GUID *)&gSetupDataPkgGenericProfileGuid)), Size))
+          ((0 != CompareMem (&ActiveProfileGuid, (EFI_GUID *)&gSetupDataPkgGenericProfileGuid, Size))))
       {
         DEBUG ((DEBUG_ERROR, "%a Invalid profile GUID received, defaulting to default profile\n", __FUNCTION__));
         CopyMem (&ActiveProfileGuid, (EFI_GUID *)&gSetupDataPkgGenericProfileGuid, Size);
