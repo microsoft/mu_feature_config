d693d9d55972935a4f2b3f1d6eab5c5789bca882
SeolforHsieh <mhsieh2@lenovo.com>
2022-12-06 16:36:33 +0800

    Update ConfProfileMgrDxeUnitTest.c

--- SetupDataPkg/ConfProfileMgrDxe/UnitTest/ConfProfileMgrDxeUnitTest.c
+++ SetupDataPkg/ConfProfileMgrDxe/UnitTest/ConfProfileMgrDxeUnitTest.c
@@ -764,6 +764,8 @@ ConfProfileMgrDxeShouldWriteReceivedProfileAndReset (
   expect_value (MockSetVariable, Data, NULL);
   will_return (MockSetVariable, EFI_SUCCESS);
 
+  will_return (IsSystemInManufacturingMode, FALSE);
+
   // set var
   expect_memory (MockSetVariable, VariableName, mKnown_Good_VarList_Names[8], StrSize (mKnown_Good_VarList_Names[8]));
   expect_memory (MockSetVariable, VendorGuid, &mKnown_Good_Xml_Guid, sizeof (EFI_GUID));
@@ -884,6 +886,8 @@ ConfProfileMgrDxeShouldWriteCachedProfileAndReset (
   expect_value (MockSetVariable, DataSize, 0);
   expect_value (MockSetVariable, Data, NULL);
   will_return (MockSetVariable, EFI_SUCCESS);
+
+  will_return (IsSystemInManufacturingMode, FALSE);
 
   // set var
   expect_memory (MockSetVariable, VariableName, mKnown_Good_VarList_Names[8], StrSize (mKnown_Good_VarList_Names[8]));
@@ -1011,6 +1015,8 @@ ConfProfileMgrDxeShouldWriteGenericProfileAndReset (
   expect_value (MockSetVariable, DataSize, 0);
   expect_value (MockSetVariable, Data, NULL);
   will_return (MockSetVariable, EFI_SUCCESS);
+
+  will_return (IsSystemInManufacturingMode, FALSE);
 
   // set var
   expect_memory (MockSetVariable, VariableName, mKnown_Good_VarList_Names[8], StrSize (mKnown_Good_VarList_Names[8]));
