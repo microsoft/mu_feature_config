5d0d969f5a0dff4bf86dfbba45217a67502d45f7
Vivian Nowka-Keane <vnowkakeane@gmail.com>
2023-11-17 13:58:22 -0800

    Add ConfigKnobShim GoogleTests (#254)
    ## Description
    
    Original PR: https://github.com/microsoft/mu_feature_config/pull/200
    
    Google Tests for ConfigKnobShimDxeLib and ConfigKnobShimPeiLib
    ConfigKnobShimPeiLib is consuming the PPI mock from mu_basecore
    (https://github.com/microsoft/mu_basecore/pull/579), so merging this is
    pending the mu_basecore PR being merged. Pipelines will fail in the
    meantime.
    
    - [ ] Impacts functionality?
    - **Functionality** - Does the change ultimately impact how firmware
    functions?
    - Examples: Add a new library, publish a new PPI, update an algorithm,
    ...
    - [ ] Impacts security?
    - **Security** - Does the change have a direct security impact on an
    application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
    - **Breaking change** - Will anyone consuming this change experience a
    break
        in build or boot behavior?
    - Examples: Add a new library class, move a module to a different repo,
    call
        a function in a new library class in a pre-existing module, ...
    - [x ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
    - **Documentation** - Does the change contain explicit documentation
    additions
        outside direct code modifications (and comments)?
    - Examples: Update readme file, add feature readme file, link to
    documentation
        on an a separate Web page, ...
    
    ## How This Was Tested
    
    Ran the tests using stuart_ci_build.
    
    ## Integration Instructions
    
    N/A

---
+++ SetupDataPkg/Library/ConfigKnobShimLib/ConfigKnobShimDxeLib/GoogleTest/ConfigKnobShimDxeLibGoogleTest.cpp
@@ -0,0 +1,175 @@
+/** @file ConfigKnobShimDxeLibGoogleTest.cpp
+  Unit tests for the ConfigKnobShimDxeLib using GoogleTest
+
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/GoogleTestLib.h>
+#include <GoogleTest/Library/MockUefiRuntimeServicesTableLib.h>
+extern "C" {
+#include <Uefi.h>
+#include <Library/BaseLib.h>
+#include <Library/DebugLib.h>
+#include <Library/ConfigKnobShimLib.h>
+}
+
+#define CONFIG_KNOB_GUID  {0x52d39693, 0x4f64, 0x4ee6, {0x81, 0xde, 0x45, 0x89, 0x37, 0x72, 0x78, 0x55}}
+
+using namespace testing;
+
+///////////////////////////////////////////////////////////////////////////////
+class GetConfigKnobOverrideFromVariableStorageTest : public Test
+{
+protected:
+StrictMock<MockUefiRuntimeServicesTableLib> RtServicesMock;
+EFI_STATUS Status;
+EFI_GUID ConfigKnobGuid;
+CHAR16 *ConfigKnobName;
+UINT64 ProfileDefaultValue;
+UINTN ProfileDefaultSize;
+UINT64 VariableData;
+UINT64 ConfigKnobData;
+
+// Redefining the Test class's SetUp function for test fixtures.
+void
+SetUp (
+  ) override
+{
+  ConfigKnobGuid      = CONFIG_KNOB_GUID;
+  ConfigKnobName      = (CHAR16 *)L"MyDeadBeefDelivery";
+  ProfileDefaultValue = 0xDEADBEEFDEADBEEF;
+  ProfileDefaultSize  = sizeof (ProfileDefaultValue);
+  VariableData        = 0xBEEF7777BEEF7777;
+  ConfigKnobData      = ProfileDefaultValue;
+}
+};
+
+//
+// Fail to find a cached config knob policy and fail to fetch config knob from
+// variable storage. Then, set the profile default value.
+//
+TEST_F (GetConfigKnobOverrideFromVariableStorageTest, VariableStorageSmallBufferFailure) {
+  // Expect the first GetVariable call to set the correct size
+  // Expect the second call to return an EFI_DEVICE_ERROR
+  EXPECT_CALL (
+    RtServicesMock,
+    gRT_GetVariable (
+      Char16StrEq ((CHAR16 *)L"MyDeadBeefDelivery"), // Example of using Char16 matcher
+      _,
+      _,
+      _,
+      _
+      )
+    )
+    .WillOnce (
+       DoAll (
+         SetArgPointee<3>(sizeof (VariableData)),
+         Return (EFI_BUFFER_TOO_SMALL)
+         )
+       )
+    .WillOnce (
+       Return (EFI_NOT_FOUND)
+       );
+
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+
+  ASSERT_EQ (Status, EFI_NOT_FOUND);
+  ASSERT_EQ (ConfigKnobData, ProfileDefaultValue);
+}
+
+//
+// With no cached config knob policy, successfully fetch config knob from
+// variable storage. Then, create cached policy.
+//
+TEST_F (GetConfigKnobOverrideFromVariableStorageTest, VariableStorageSuccess) {
+  // Expect the first GetVariable call to get size
+  EXPECT_CALL (
+    RtServicesMock,
+    gRT_GetVariable
+    )
+    .WillOnce (
+       DoAll (
+         SetArgPointee<3>(sizeof (VariableData)),
+         Return (EFI_BUFFER_TOO_SMALL)
+         )
+       );
+
+  // Expect the second getVariable call to update data
+  // NOTE: in this case, could also simply do another .WillOnce call. But, wanted to show a little variety
+  EXPECT_CALL (
+    RtServicesMock,
+    gRT_GetVariable (
+      _,
+      _,
+      _,
+      _,
+      NotNull ()
+      )
+    )
+    .WillOnce (
+       DoAll (
+         SetArgPointee<3>(sizeof (VariableData)),
+         SetArgBuffer<4>(&VariableData, sizeof (VariableData)),
+         Return (EFI_SUCCESS)
+         )
+       );
+
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+
+  ASSERT_EQ (Status, EFI_SUCCESS);
+  ASSERT_EQ (VariableData, ConfigKnobData);
+}
+
+//
+// Fail to find a cached config knob policy and fail to fetch config knob from
+// variable storage. Then, set the profile default value.
+//
+TEST_F (GetConfigKnobOverrideFromVariableStorageTest, VariableStorageNotFoundFailure) {
+  // Expect the first GetVariable call to get size to fail
+  EXPECT_CALL (
+    RtServicesMock,
+    gRT_GetVariable
+    )
+    .WillOnce (
+       Return (EFI_NOT_FOUND)
+       );
+
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+
+  ASSERT_EQ (Status, EFI_NOT_FOUND);
+  ASSERT_EQ (ConfigKnobData, ProfileDefaultValue);
+}
+
+/*
+  Fail to find a cached config knob policy and succeed to fetch config knob from
+  variable storage. Fail to match variable size with profile default size. Then, set the profile default value.
+*/
+TEST_F (GetConfigKnobOverrideFromVariableStorageTest, VariableStorageSizeFailure) {
+  // Expect the first GetVariable call to get (non-matching) size
+  EXPECT_CALL (
+    RtServicesMock,
+    gRT_GetVariable
+    )
+    .WillOnce (
+       DoAll (
+         SetArgPointee<3>(sizeof (UINT32)),
+         Return (EFI_BUFFER_TOO_SMALL)
+         )
+       );
+
+  Status = GetConfigKnobOverride (&ConfigKnobGuid, ConfigKnobName, (VOID *)&ConfigKnobData, ProfileDefaultSize);
+  ASSERT_EQ (Status, EFI_BAD_BUFFER_SIZE);
+  ASSERT_EQ (ConfigKnobData, ProfileDefaultValue);
+}
+
+int
+main (
+  int   argc,
+  char  *argv[]
+  )
+{
+  InitGoogleTest (&argc, argv);
+  return RUN_ALL_TESTS ();
+}
