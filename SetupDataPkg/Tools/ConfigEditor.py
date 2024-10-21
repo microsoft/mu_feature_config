67dbc85ac42c65e3e8fe6153d34116b81b13d734
HungYu Hsiao (UST Global Inc) <v-huhsiao@microsoft.com>
2024-10-21 14:57:03 +0800

    load and show mfci pocliy

--- SetupDataPkg/Tools/ConfigEditor.py
+++ SetupDataPkg/Tools/ConfigEditor.py
@@ -531,10 +531,13 @@ class application(tkinter.Frame):
             Manufaturing_enabled = (char_ext2_data & (0x1 << 6)) >> 6
             print(f"Manufaturing : {Manufaturing_enabled:02X}")
 
-        self.canvas = tkinter.Canvas(master, width=150, height=50, bg="white")
-        self.canvas.place(relx=1.0, rely=1.0, x=-10, y=-10, anchor='se')
-        self.canvas.create_rectangle(10, 10, 140, 40, fill="blue", outline="black")
-        self.canvas.create_text(75, 25, text=f"Manufacturing Mode: {Manufaturing_enabled}", fill="white")
+        # get mfci policy
+        mfci_policy_result = BoardMiscInfo.get_mfci_policy()
+
+
+        self.canvas = tkinter.Canvas(master, width=170, height=50, bg=master['bg'], highlightthickness=0)
+        self.canvas.place(relx=1.0, rely=1.0, x=0, y=0, anchor='se')
+        self.canvas.create_text(85, 20, text=f"Manufacturing Mode: {Manufaturing_enabled}\nMfci Policy: {mfci_policy_result}", fill="black", font=("Helvetica", 10, "bold"))
 
         idx = 0
 
