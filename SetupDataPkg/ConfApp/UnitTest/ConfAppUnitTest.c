9f894a4fd217c4f07dc6e2acfdd6449a0c764453
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2025-06-23 09:20:48 -0700

    Removed one KeyData2, rearranged MockResetSystem

--- SetupDataPkg/ConfApp/UnitTest/ConfAppUnitTest.c
+++ SetupDataPkg/ConfApp/UnitTest/ConfAppUnitTest.c
@@ -393,15 +393,16 @@ ConfAppEntrySelect1 (
   KeyData2.Key.ScanCode    = SCAN_NULL;
   will_return (MockReadKey, &KeyData2);
 
-  KeyData2.Key.UnicodeChar = 'y';
-  KeyData2.Key.ScanCode    = SCAN_NULL;
-  will_return (MockReadKey, &KeyData2);
+  expect_value (MockResetSystem, ResetType, EfiResetCold);
+
+  // KeyData2.Key.UnicodeChar = 'y';
+  // KeyData2.Key.ScanCode    = SCAN_NULL;
+  // will_return (MockReadKey, &KeyData2);
 
   KeyData3.Key.UnicodeChar = CHAR_NULL;
   KeyData3.Key.ScanCode    = SCAN_ESC;
   will_return (MockReadKey, &KeyData3);
 
-  expect_value (MockResetSystem, ResetType, EfiResetCold);
   // expect_value (MockResetSystem, ResetType, EfiResetCold);
   // expect_value (MockResetSystem, ResetType, EfiResetCold);
 
